6b9f49cec60afaf77dee7ef626e272b55f91db1a
SirMorfield <joppe.koers@gmail.com>
2019-02-01 14:24:26 +0100

    oops

--- server/app.js
+++ server/app.js
@@ -40,13 +40,13 @@ io.on('connection', function (socket) {
   })
 });
 
-// exec('DISPLAY=:0 chromium-browser --disable-notifications --disable-infobars --start-fullscreen --app=http://localhost:8080', (err, stdout, stderr) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
+exec('DISPLAY=:0 chromium-browser --disable-notifications --disable-infobars --start-fullscreen --app=http://localhost:8080', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
 
 console.log('running');
