d73fd827d7d703cffb3da6eda7e4d98f6721721f
Roembol <36731661+roembol2000@users.noreply.github.com>
2025-09-06 00:59:46 +0200

    wip ui revamp--unsure what i was smoking..

--- public/js/main.js
+++ public/js/main.js
@@ -12,15 +12,42 @@ function selectNextGame(direction) {
 	if (newSelectedGame < 0) {
 		newSelectedGame = gamesLength;
 	}
+	
+	const selectedGameElement = document.getElementsByClassName("game")[newSelectedGame];
+	selectedGameElement.className = 'game selected';
 
-	document.getElementsByClassName("game")[newSelectedGame].className = 'game selected';
+	// const gamesContainer = document.getElementsByClassName("gamesContainer")[0];
+	// if (newSelectedGame == 0) {
+	// 	gamesContainer.scrollTo(0, 0);
+	// } else if (newSelectedGame == gamesLength) {
+	// 	gamesContainer.scrollTo(gamesContainer.scrollWidth, 0)
+	// } else {
+	// 	selectedGameElement.parentNode.scrollWidth = selectedGameElement.offsetLeft;
+	// 	// selectedGameElement.scrollIntoView();
+	// 	// document.documentElement.scrollTo(0, 0)
+	// 	// gamesContainer.scrollTo(selectedGameElement.offsetX, 0)
+	// }
+
+	const allGames = document.getElementsByClassName("game");
+
+	for (game of allGames) {
+		game.style.transform = `translateX(${newSelectedGame * -361}px)`;
+	}
+
+	selectedGameElement.style.transform = `translate(${newSelectedGame * -361}px, -15px)`;
+	
 	selectedGame = newSelectedGame;
 }
 
 function startSelectedGame() {
 	let location = '/games/' + document.getElementsByClassName('selected')[0].id;
 	location = location.replace('?', '');
-	window.location.replace(location);
+
+	document.body.classList.add("blurFade");
+
+	setTimeout(() => {
+		window.location.replace(location);
+	}, 500);
 }
 
 window.onload = function () {
@@ -40,4 +67,5 @@ window.onload = function () {
 		}
 	});
 	document.getElementsByClassName("game")[0].className = 'game selected'
+	document.body.classList.remove("blurFade");
 }
